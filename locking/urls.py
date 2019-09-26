ef7c7d2df1d33b58cb880002575ef0566da845d8
Carlo Oliva <carlo.oliva@surehire.com>
2019-09-26 10:32:52 -0600

    Remove warning

--- locking/urls.py
+++ locking/urls.py
@@ -1,13 +1,6 @@
 from django.conf.urls import url
 from django.views.i18n import JavaScriptCatalog
 
-from warnings import warn
-
-
-warn("The use of 'locking.urls' is deprecated and is no longer needed.",
-    DeprecationWarning)
-
-
 # We need at least one url inside urlpatterns to keep include('locking.urls')
 # from throwing an exception
 urlpatterns = [
